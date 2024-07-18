a4c5dc47c56d2418fc9e26b4eef68d29fdf479c5
Umut Yenidil <umut.yenidil9@gmail.com>
2024-07-18 15:25:02 +0300

    admin ve user namespace'leri tanimlandi

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,14 +5,26 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->group("/api", static function (RouteCollection $routes) {
-    $routes->group("v1", static function (RouteCollection $routes) {
-        $routes->group("products", static function (RouteCollection $routes) {
-            $routes->get("", "ProductController::readProducts");
-            $routes->post("", "ProductController::createProduct");
-            $routes->get(":id", "ProductController::readProduct");
-            $routes->put(":id", "ProductController::updateProduct");
-            $routes->delete(":id", "ProductController::deleteProduct");
+$routes->group("api", ["namespace" => "App\Controllers\Api"], function (RouteCollection $routes) {
+    $routes->group("v1", ["namespace" => "App\Controllers\Api\V1"], function (RouteCollection $routes) {
+        $routes->group("admin", ["namespace" => "App\Controllers\Api\V1\Admin"], function (RouteCollection $routes) {
+            $routes->group("auth", function (RouteCollection $routes) {
+                $routes->post("sign-in", "AuthController::signIn");
+            });
+
+            $routes->group("products", function (RouteCollection $routes) {
+                $routes->get("", "ProductController::readProducts");
+                $routes->post("", "ProductController::createProduct");
+                $routes->get(":id", "ProductController::readProduct");
+                $routes->put(":id", "ProductController::updateProduct");
+                $routes->delete(":id", "ProductController::deleteProduct");
+            });
+        });
+        $routes->group("user", ["namespace" => "App\Controllers\Api\V1\User"], function (RouteCollection $routes) {
+            $routes->group("products", function (RouteCollection $routes) {
+                $routes->get("", "ProductController::readProducts");
+                $routes->get(":id", "ProductController::readProduct");
+            });
         });
     });
 });
---
+++ app/Controllers/Api/V1/User/ProductController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Controllers\Api\V1\User;
+
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\RESTful\ResourceController;
+
+use Ramsey\Uuid\Uuid;
+use App\Models\ProductModel;
+
+class ProductController extends ResourceController
+{
+    private $productModel;
+
+    public function __construct()
+    {
+        $this->productModel = new ProductModel();
+    }
+
+
+    /**
+     * reads all products from database
+     * METHOD: GET
+     * @return ResponseInterface
+     */
+    public function readProducts(): ResponseInterface
+    {
+        $products = $this->productModel->findAll();
+
+        return $this->respond([
+            "code" => "200",
+            "message" => "success",
+            "data" => [
+                "products" => $products,
+            ],
+        ], 200);
+    }
+
+    /**
+     * read user with id from database
+     * METHOD: GET
+     * @return ResponseInterface
+     */
+    public function readProduct(): ResponseInterface
+    {
+        $productId = "5905c570-23af-4648-ad61-86a03e869fc7";
+
+        $product = $this->productModel->find($productId);
+
+        return $this->respond([
+            "code" => "200",
+            "message" => "success",
+            "data" => [
+                "products" => $product,
+            ],
+        ], 200);
+    }
+
+    public function createProduct(): ResponseInterface
+    {
+        $incomingData = $this->request->getJSON(true)["data"];
+
+        $productUuid = Uuid::uuid4()->toString();
+        $data = [
+            "id" => $productUuid,
+            "name" => $incomingData["name"],
+            "description" => $incomingData["description"],
+        ];
+
+        $isProductCreated = $this->productModel->insert($data);
+
+        if ($isProductCreated) {
+            return $this->respond([
+                "code" => "201",
+                "message" => "success",
+                "data" => $data,
+            ], 201);
+        } else {
+            return $this->respond([
+                "code" => "500",
+                "message" => "Something went wrong",
+            ], 500);
+        }
+
+
+    }
+
+    public function updateProduct(): ResponseInterface
+    {
+        $productId = "";
+
+        $incomingData = $this->request->getJSON(true)["data"];
+
+        $isProductUpdated = $this->productModel->update([
+            "id" => $productId,
+        ], $incomingData);
+
+        if ($isProductUpdated) {
+            return $this->respond([
+                "code" => "204",
+                "message" => "success",
+            ], 204);
+        } else {
+            return $this->respond([
+                "code" => "304",
+                "message" => "Something went wrong",
+            ], 304);
+        }
+
+    }
+
+
+    public function deleteProduct(): ResponseInterface
+    {
+        $productId = '';
+
+        $isProductDeleted = $this->productModel->delete($productId);
+
+        if ($isProductDeleted) {
+            return $this->respond([
+                "code" => "204",
+                "message" => "success",
+            ], 204);
+        } else {
+            return $this->respond([
+                "code" => "500",
+                "message" => "Something went wrong",
+            ], 500);
+        }
+    }
+}
+
